17ce766cd8fcb5e60e45cd754660c86025697d86
andresunda <and_ua@hotmail.com>
2025-03-14 15:15:25 -0600

    Changes to function on vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
-
 export default function Page() {
   return (
     
